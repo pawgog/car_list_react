8e99a798738d630775e8eb1c402abbbe291db4d2
pawgog <p.gogola@interia.pl>
2020-08-26 15:57:38 +0200

    Set react app and integrate api.

---
+++ src/components/Root.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Root extends React.Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+ 
+  }
+
+  render() {
+
+    return (
+      <>
+        <h1>Test</h1>
+      </>
+    )
+  }
+}
+
+export default Root;
